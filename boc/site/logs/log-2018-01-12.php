e085ef1acb6ff9cdb3144ad0651e0e8d786b2631
chenmiaowei <251949141@qq.com>
2018-01-12 13:12:39 +0800

    修改

--- boc/site/logs/log-2018-01-12.php
+++ boc/site/logs/log-2018-01-12.php
@@ -291,3 +291,7 @@ ERROR - 2018-01-12 13:11:28 --> Language file contains no data: language/zh_CN/c
 ERROR - 2018-01-12 13:11:28 --> 404 Page Not Found --> 
 ERROR - 2018-01-12 13:11:28 --> Severity: Warning  --> include_once(/Users/air/Documents/personal/skycup/boc/site/views/404.php): failed to open stream: No such file or directory /Users/air/Documents/personal/skycup/boc/site/errors/error_404.php 1
 ERROR - 2018-01-12 13:11:28 --> Severity: Warning  --> include_once(): Failed opening '/Users/air/Documents/personal/skycup/boc/site/views/404.php' for inclusion (include_path='.:') /Users/air/Documents/personal/skycup/boc/site/errors/error_404.php 1
+ERROR - 2018-01-12 13:12:17 --> Severity: Notice  --> Only variable references should be returned by reference /Users/air/Documents/personal/skycup/boc/libs/ci/core/Common.php 264
+ERROR - 2018-01-12 13:12:17 --> Language file contains no data: language/zh_CN/common_lang.php
+ERROR - 2018-01-12 13:12:18 --> Severity: Notice  --> Only variable references should be returned by reference /Users/air/Documents/personal/skycup/boc/libs/ci/core/Common.php 264
+ERROR - 2018-01-12 13:12:18 --> Language file contains no data: language/zh_CN/common_lang.php
