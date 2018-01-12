34cff1133768c50b27fe74fa1852cdca870174ca
chenmiaowei <251949141@qq.com>
2018-01-12 10:58:31 +0800

    修改

--- boc/admins/logs/log-2018-01-12.php
+++ boc/admins/logs/log-2018-01-12.php
@@ -11,3 +11,9 @@ ERROR - 2018-01-12 10:49:18 --> Could not find the language line "conent"
 ERROR - 2018-01-12 10:49:18 --> Severity: Notice  --> Only variable references should be returned by reference /Users/air/Documents/personal/skycup/boc/libs/ci/core/Common.php 264
 ERROR - 2018-01-12 10:49:18 --> Could not find the language line "conent"
 ERROR - 2018-01-12 10:49:19 --> Severity: Notice  --> Only variable references should be returned by reference /Users/air/Documents/personal/skycup/boc/libs/ci/core/Common.php 264
+ERROR - 2018-01-12 10:57:12 --> Severity: Notice  --> Only variable references should be returned by reference /Users/air/Documents/personal/skycup/boc/libs/ci/core/Common.php 264
+ERROR - 2018-01-12 10:57:12 --> Could not find the language line "conent"
+ERROR - 2018-01-12 10:57:14 --> Severity: Notice  --> Only variable references should be returned by reference /Users/air/Documents/personal/skycup/boc/libs/ci/core/Common.php 264
+ERROR - 2018-01-12 10:57:14 --> Could not find the language line "conent"
+ERROR - 2018-01-12 10:57:15 --> Severity: Notice  --> Only variable references should be returned by reference /Users/air/Documents/personal/skycup/boc/libs/ci/core/Common.php 264
+ERROR - 2018-01-12 10:57:15 --> Could not find the language line "conent"
