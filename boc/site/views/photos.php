d7b3b70cca5f52f969b16533865a66548bea99d5
chenmiaowei <251949141@qq.com>
2018-01-16 22:41:23 +0800

    修改

--- boc/site/views/photos.php
+++ boc/site/views/photos.php
@@ -2,7 +2,7 @@
 !isset($reg[0]) and show_404();
 
 $CI->load->model('gallery_model', 'mgallery');
-$list = $CI->mgallery->get_all(array(), 'id,photo,pic,title,position,content');
+$list = $CI->mgallery->get_all(array('cid' => 24), 'id,photo,pic,title,position,content');
 //$list = $CI->mgallery->get_all(array('aid' => $reg[0]), 'id,photo,pic,title,position,content');
 
 
